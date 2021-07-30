7d4655f4ba97d8eb9f4fc29d4e158adfce768a15
Utkarsh-raj-73 <utkarsh@salesken.ai>
2021-07-30 17:15:30 +0530

    username

--- server.js
+++ server.js
@@ -14,14 +14,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use(session({
-  secret: 'shubhamLohra',
-  resave: false,
-  saveUninitialized: false
+    secret: 'shubhamLohra',
+    resave: false,
+    saveUninitialized: false
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+const uri = "mongodb+srv://lohra:accha_paSSword123@cluster0.y6dcv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-mongoose.connect("mongodb://localhost:27017/farmerDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 mongoose.set("useCreateIndex", true);
 
@@ -38,7 +39,7 @@ mongoose.set("useCreateIndex", true);
 
 //     },
 
-//     adhar:{
+//     Aadhaar:{
 //       type:String,
 //       required:true
 
@@ -61,38 +62,38 @@ mongoose.set("useCreateIndex", true);
 // };
 
 const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-  address: String,
-  username: String,
-  phone: Number,
-  Aadhar: String,
-  secret: String
+    email: String,
+    password: String,
+    address: String,
+    username: String,
+    phone: Number,
+    AAadhaar: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
 
 const farmerSchema = {
 
-  adharno: {
-    type: String,
-    required: true
-  },
+    Aadhaarno: {
+        type: String,
+        required: true
+    },
 
-  crops: {
-    type: String,
-    required: true
-  },
+    crops: {
+        type: String,
+        required: true
+    },
 
-  quantity: {
-    type: String,
-    required: true
-  },
+    quantity: {
+        type: String,
+        required: true
+    },
 
-  price: {
-    type: String,
-    required: true
-  }
+    price: {
+        type: String,
+        required: true
+    }
 
 
 };
@@ -105,177 +106,165 @@ passport.deserializeUser(User.deserializeUser());
 
 const Farmer = new mongoose.model("Farmer", farmerSchema);
 
-app.get("/", function (req, res) {
-  if (req.isAuthenticated()) {
-    console.log(req.user.username);
-    const username = req.user.username;
-    res.render("page3",{username});
-  }
-  else {
-    res.render("page1");
-  }
-});
-
-app.get("/login", function (req, res) {
-  res.render("login");
-});
-
-app.get("/register", function (req, res) {
-  res.render("register");
-});
-
-
-app.listen(process.env.port || 3000, function (req, res) {
-  console.log("server started ")
-});
-
-app.post("/register", function (req, res) {
-
-  User.register({
-    username: req.body.name, 
-    phone: req.body.number,
-    adhar: req.body.adhar,
-    address: req.body.adress,
-    email: req.body.username,
-  }, req.body.password, function (err, user) {
-    if (err) {
-      console.log(err);
-      console.log(user);
-      res.redirect("/register");
+app.get("/", function(req, res) {
+    if (req.isAuthenticated()) {
+        console.log(req.user.username);
+        const username = req.user.username;
+        res.render("page3", { username });
     } else {
-      console.log(user);
-      const authenticate = User.authenticate();
-      authenticate("username", "password", function (err, result) {
+        res.render("page1");
+    }
+});
+
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+
+app.listen(process.env.port || 3000, function(req, res) {
+    console.log("server started ")
+});
+
+app.post("/register", function(req, res) {
+
+    User.register({
+        username: req.body.email,
+        phone: req.body.number,
+        Aadhaar: req.body.Aadhaar,
+        address: req.body.adress,
+        name: req.body.name,
+    }, req.body.password, function(err, user) {
         if (err) {
-          res.redirect("/register");
+            console.log(err);
+            console.log(user);
+            res.redirect("/register");
+        } else {
+            console.log(user);
+            const authenticate = User.authenticate();
+            authenticate("username", "password", function(err, result) {
+                    if (err) {
+                        res.redirect("/register");
+                    } else {
+                        res.redirect("/login");
+                    }
+                })
+                //   passport.authenticate("local")(req, res, function () {
+                //     res.redirect("/login");
+
+            //   });
         }
-        else {
-          res.redirect("/login");
+    });
+    // const newUser = new User({
+    //   username: req.body.name,
+    //   phone: req.body.number,
+    //   Aadhaar: req.body.Aadhaar,
+    //   address: req.body.adress,
+    //   email: req.body.username,
+    //   password: req.body.password
+    // });
+    // newUser.save();
+});
+
+
+app.post("/post", function(req, res) {
+    const newFarmer = new Farmer({
+
+        Aadhaarno: req.body.Aadhaarno,
+        crops: req.body.crops,
+        quantity: req.body.quantity,
+        price: req.body.price
+    });
+
+    newFarmer.save(function(err) {
+        if (err) {
+            console.log(err);
+            res.render("errormessage");
+        } else {
+            console.log("updated successfully");
+            res.render("post");
         }
-      })
-      //   passport.authenticate("local")(req, res, function () {
-      //     res.redirect("/login");
-
-      //   });
-    }
-  });
-  // const newUser = new User({
-  //   username: req.body.name,
-  //   phone: req.body.number,
-  //   adhar: req.body.adhar,
-  //   address: req.body.adress,
-  //   email: req.body.username,
-  //   password: req.body.password
-  // });
-  // newUser.save();
-});
-
-
-app.post("/post", function (req, res) {
-  const newFarmer = new Farmer({
-
-    adharno: req.body.adharno,
-    crops: req.body.crops,
-    quantity: req.body.quantity,
-    price: req.body.price
-  });
-
-  newFarmer.save(function (err) {
-    if (err) {
-      console.log(err);
-      res.render("errormessage");
-    }
-    else {
-      console.log("updated successfully");
-      res.render("post");
-    }
-  });
+    });
 
 });
 
-app.post("/login", function (req, res) {
+app.post("/login", function(req, res) {
 
-  console.log(req);
-  const user = new User({
-    username: req.body.name,
-    password: req.body.password
-  })
+    console.log(req);
+    const user = new User({
+        username: req.body.email,
+        password: req.body.password
+    })
 
-  req.login(user, function (err,user) {
-    if (err) {
-      console.log(err);
-      res.render("login");
-    }
-   
-    else {
-      if (req.isAuthenticated()) {
-        console.log(user);
-        const username = req.body.name;
-        res.render("page3",{username});
-      }
-      else {
-        console.log(user);
-        res.redirect("/login");
-      }
+    req.login(user, function(err, user) {
+        if (err) {
+            console.log(err);
+            res.render("login");
+        } else {
+            if (req.isAuthenticated()) {
+                console.log(user);
+                const username = req.body.email;
+                res.render("page3", { username });
+            } else {
+                console.log(user);
+                res.redirect("/login");
+            }
 
 
-    }
-  })
+        }
+    })
 });
 
-app.post("/page3",function(req,res)
-{
-  const crop = req.body.box;
-  res.render("page4",{crop});
+app.post("/page3", function(req, res) {
+    const crop = req.body.box;
+    res.render("page4", { crop });
 });
 
-app.post("/page4",function(req,res)
-{
-  const crop = req.body.box;
-  res.render("page4",{crop});
+app.post("/page4", function(req, res) {
+    const crop = req.body.box;
+    res.render("page4", { crop });
 });
 
 
-app.get("/page3", function (req, res) {
-  res.render("page3");
+app.get("/page3", function(req, res) {
+    res.render("page3");
 })
 
-app.get("/buyer", function (req, res) {
-  res.render("buyer");
+app.get("/buyer", function(req, res) {
+    res.render("buyer");
 });
 
-app.get("/page4",function(req,res)
-{
-  res.render("page4");
+app.get("/page4", function(req, res) {
+    res.render("page4");
 });
 
-app.get("/cart", function (req, res) {
-  res.render("cart");
+app.get("/cart", function(req, res) {
+    res.render("cart");
 })
 
-app.get("/seller", function (req, res) {
-  res.render("seller");
+app.get("/seller", function(req, res) {
+    res.render("seller");
 })
-app.get("/errormessage", function (req, res) {
-  res.render("errormessage");
-})
-
-app.get("/post", function (req, res) {
-  res.render("post");
+app.get("/errormessage", function(req, res) {
+    res.render("errormessage");
 })
 
-app.get("/profile", function (req, res) {
-  res.render("profile");
+app.get("/post", function(req, res) {
+    res.render("post");
 })
 
-app.get("/home", function (req, res) {
-  res.render("home");
+app.get("/profile", function(req, res) {
+    res.render("profile");
 })
 
-app.get("/logout",function(req,res)
-{
-  req.logout();
-  res.render("page1");
+app.get("/home", function(req, res) {
+    res.render("home");
 })
 
-
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.render("page1");
+})
